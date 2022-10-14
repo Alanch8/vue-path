88acb600a17aa13acfcbb1fc3b010adcfb534e9c
Aitor Lancharro <aitor.lancharro@gmail.com>
2022-10-14 09:19:44 +0200

    02-template

---
+++ 01-basics-js/src/basics/02-template.js
@@ -0,0 +1,12 @@
+const nombre = 'Aitor'
+const apellido = 'Lancharro'
+
+const nombreCompleto = `${ nombre } ${ apellido }`
+
+console.log(`${1 + 1}`);
+
+function getSaludo ( nombre ) {
+    return 'Hola '+ nombre
+}
+
+console.log(`Saludo: ${getSaludo(nombre)}` );
