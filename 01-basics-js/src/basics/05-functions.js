1f2a1fb94f2acce3f441817c1abf177a62f7c98d
Aitor Lancharro <aitor.lancharro@gmail.com>
2022-10-15 14:36:49 +0200

    functions

---
+++ 01-basics-js/src/basics/05-functions.js
@@ -0,0 +1,33 @@
+
+
+/* function saludar(xyz) {
+    return `Hola ${xyz}`
+} */
+
+/* const saludar = ( xyz ) => {
+  return `Hola ${xyz}`;
+} */
+
+const saludar = (nombre) => `Hola ${nombre}`
+
+const nombre = 'Tony'
+
+// console.log(saludar(nombre));
+
+const getUser = () => ({ uid: 'ABC123', username: 'Tony001' })
+
+// console.log(getUser());
+
+const heroes = [{
+    id: 1,
+    name: 'Batman'
+},{
+    id:2,
+    name: 'Superman'
+}]
+
+// const exist = heroes.some( ( heroe ) => heroe.id === 1 );
+
+const {name} = heroes.find((heroe) => heroe.id === 1);
+
+console.log(name);
