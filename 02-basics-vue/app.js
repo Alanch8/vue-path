f2e684bb9ecf8422f6fdbc21c8d8577b04403763
Aitor Lancharro <aitor.lancharro@gmail.com>
2022-10-25 13:11:02 +0200

    Introducción a los eventos

--- 02-basics-vue/app.js
+++ 02-basics-vue/app.js
@@ -10,6 +10,17 @@ const app = Vue.createApp({
             quote: "I'm Batman",
             author: "Bruce Wayne"
         }
+    },
+    methods: {
+        changeQuote(){
+            console.log('Hello world')
+            this.author = 'Aito Lancharro'
+
+            this.capitalize()
+        },
+        capitalize(){
+            this.quote = this.quote.toUpperCase()
+        }
     }
 
 })
