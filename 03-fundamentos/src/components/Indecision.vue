5c310dde4a82639f750c4d5c78354537a60e4d29
Aitor Lancharro <aitor.lancharro@gmail.com>
2022-11-03 13:36:11 +0100

    Watch - Observar cambios en una propiedad reactiva

--- 03-fundamentos/src/components/Indecision.vue
+++ 03-fundamentos/src/components/Indecision.vue
@@ -2,17 +2,30 @@
   <img src="https://via.placeholder.com/250" alt="bg" />
   <div class="bg-dark"></div>
   <div class="indecision-container">
-    <input type="text" placeholder="Hazme una pregunta!" />
+    <input type="text" v-model="question" placeholder="Hazme una pregunta!" />
     <p>Recuerda terminar con un signo de interrogación (?)</p>
     <div>
-      <h2>Seré millonario?</h2>
+      <h2>{{ question }}</h2>
       <h1>Si, No, ... Pensando</h1>
     </div>
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      question: null,
+    };
+  },
+  watch: {
+    question(value, oldValue) {
+      if (!value.includes("?")) return;
+
+      //TODO
+    },
+  },
+};
 </script>
 
 <style scoped>
