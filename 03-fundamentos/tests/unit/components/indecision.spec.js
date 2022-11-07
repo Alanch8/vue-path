d84c446aa5db0a8ed2773772e6dacf53b5c592c2
Aitor Lancharro <aitor.lancharro@gmail.com>
2022-11-07 20:37:45 +0100

    Spy y Mocks

--- 03-fundamentos/tests/unit/components/indecision.spec.js
+++ 03-fundamentos/tests/unit/components/indecision.spec.js
@@ -4,16 +4,25 @@ import Indecision from '@/components/Indecision.vue'
 describe('Indecision component', () => {
 
     let wrapper;
+    let clgSpy;
     
     beforeEach( () => {
         wrapper = shallowMount( Indecision )
+
+        clgSpy = jest.spyOn( console, 'log' )
+
     })
     
     test('debe de hacer match con el snapshot', () => {
         expect( wrapper.html() ).toMatchSnapshot()
     })
 
-    test('escribir en el input no debe de disparar nada (console.log)', () => {
+    test('escribir en el input no debe de disparar nada (console.log)', async() => {
+
+        const input = wrapper.find('input')
+        await input.setValue('Hello world')
+
+        expect( clgSpy ).toHaveBeenCalledTimes(1)
 
     })
 
