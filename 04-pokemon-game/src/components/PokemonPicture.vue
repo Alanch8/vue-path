0efe19985863c9da8cba319f74a9cc68ce269bea
Aitor Lancharro <aitor.lancharro@gmail.com>
2022-11-10 13:05:11 +0100

    Diseño de los componentes

--- 04-pokemon-game/src/components/PokemonPicture.vue
+++ 04-pokemon-game/src/components/PokemonPicture.vue
@@ -1,13 +1,38 @@
 <template>
-  <h3>Pokémon Picture</h3>
+  <div class="pokemon-container">
+    <img
+      src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg"
+      class="hidden-pokemon"
+      alt="pokémon"
+    />
+    <!-- <img
+      src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg"
+      class="fade-in"
+      alt="pokémon"
+    /> -->
+  </div>
 </template>
 
 <script>
-export default {
-
-}
+export default {};
 </script>
 
-<style>
-
+<style scoped>
+.pokemon-container {
+  height: 200px;
+  display: flex;
+  justify-content: center;
+}
+img {
+  height: 200px;
+  position: absolute;
+  user-select: none;
+  -moz-user-select: none;
+  -ms-user-select: none;
+  -webkit-user-drag: none;
+  -webkit-user-select: none;
+}
+.hidden-pokemon {
+  filter: brightness(0);
+}
 </style>
