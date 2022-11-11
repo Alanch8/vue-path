e0e90e4bba61e8d0dae2180eb1212e5787bfa895
Aitor Lancharro <aitor.lancharro@gmail.com>
2022-11-11 12:10:00 +0100

    Seleccionar un pokémon aleatoriamente

--- 04-pokemon-game/src/pages/PokemonPage.vue
+++ 04-pokemon-game/src/pages/PokemonPage.vue
@@ -1,14 +1,17 @@
 <template>
-  <h1>Who's that Pokémon?</h1>
-  <PokemonPicture :pokemonId="133" :showPokemon="true"/>
-  <PokemonOptions :pokemons="pokemonArr" />
+  <h1 v-if="!pokemon">Wait pls...</h1>
+  <div v-else>
+    <h1>Who's that Pokémon?</h1>
+    <PokemonPicture :pokemonId="pokemon.id" :showPokemon="showPokemon" />
+    <PokemonOptions :pokemons="pokemonArr" />
+  </div>
 </template>
 
 <script>
 import PokemonPicture from "@/components/PokemonPicture.vue";
 import PokemonOptions from "@/components/PokemonOptions.vue";
 
-import getPokemonOptions from '@/helpers/getPokemonOptions'
+import getPokemonOptions from "@/helpers/getPokemonOptions";
 
 // console.log( getPokemonOptions() );
 
@@ -18,19 +21,23 @@ export default {
     PokemonOptions,
   },
   data() {
-
     return {
-      pokemonArr: []
-    }
+      pokemonArr: [],
+      pokemon: null,
+      showPokemon: false,
+    };
   },
   methods: {
     async mixPokemonArray() {
-      this.pokemonArr = await getPokemonOptions()
-    }
+      this.pokemonArr = await getPokemonOptions();
+
+      const rndInt = Math.floor(Math.random() * 4);
+      this.pokemon = this.pokemonArr[rndInt];
+    },
   },
   mounted() {
-    this.mixPokemonArray()
-  }
+    this.mixPokemonArray();
+  },
 };
 </script>
 
