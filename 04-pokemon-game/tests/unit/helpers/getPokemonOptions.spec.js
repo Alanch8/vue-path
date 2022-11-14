4ab8e8860a6ef290478e7416ad1aef36c8f890a2
Aitor Lancharro <aitor.lancharro@gmail.com>
2022-11-14 15:26:52 +0100

    getPokemonName y getPokemonOptions

--- 04-pokemon-game/tests/unit/helpers/getPokemonOptions.spec.js
+++ 04-pokemon-game/tests/unit/helpers/getPokemonOptions.spec.js
@@ -1,26 +1,68 @@
-import { getPokemon } from '@/helpers/getPokemonOptions'
+import getPokemonOptions, {
+  getPokemons,
+  getPokemonNames,
+} from "@/helpers/getPokemonOptions";
 
-describe('getPokemonOptions helpers', () => {
+describe("getPokemonOptions helpers", () => {
+  test("debe de regresar un arreglo de numeros", () => {
+    const pokemons = getPokemons();
 
-    test('debe de regresar un arreglo de numeros', () => {
+    // console.log(pokemons);
 
-        const pokemons = getPokemons()
+    expect(pokemons.lenght).toBe(650);
+    expect(pokemons[0]).toBe(1);
+    expect(pokemons[500]).toBe(501);
+    expect(pokemons[649]).toBe(650);
+  });
 
-        // console.log(pokemons);
+  test("debe de retornar un arreglo de 4 elementos con nombres de pokemons", async () => {
+    const pokemons = await getPokemonNames([1, 2, 3, 4]);
 
-        expect(pokemons.lenght).toBe(650)
-        expect(pokemons[0]).toBe(1);
-        expect(pokemons[500]).toBe(501);
-        expect(pokemons[649]).toBe(650);
+    // console.log(pokemons);
 
-    })
+    expect(pokemons).toEqual([
+      {
+        name: "bulbasur",
+        id: 1,
+      },
+      {
+        name: "ivysaur",
+        id: 2,
+      },
+      {
+        name: "venusaur",
+        id: 3,
+      },
+      {
+        name: "charmander",
+        id: 4,
+      },
+    ]);
+  });
 
-    test('debe de retornar un arreglo de 4 elementos con nombres de pokemons', () => {
+  test("getPokemonOptions debe de retornar un arreglo mezclado", async () => {
+    const pokemons = await getPokemonOptions();
 
-    })
+    // console.log(pokemons);
 
-    test('getPokemonOptions debe de retornar un arreglo mezclado', () => {
-        
-    })
-
-})
+    expect(pokemons.length).toBe(4);
+    expect(pokemons).toEqual([
+      {
+        name: expect.any(String),
+        id: expect.any(Number),
+      },
+      {
+        name: expect.any(String),
+        id: expect.any(Number),
+      },
+      {
+        name: expect.any(String),
+        id: expect.any(Number),
+      },
+      {
+        name: expect.any(String),
+        id: expect.any(Number),
+      },
+    ]);
+  });
+});
