4581516e8b9388144cf81868d9eb652543e20ae7
Aitor Lancharro <aitor.lancharro@gmail.com>
2022-11-14 17:35:42 +0100

    Pruebas con emisiones

--- 04-pokemon-game/tests/unit/components/PokemonOptions.spec.js
+++ 04-pokemon-game/tests/unit/components/PokemonOptions.spec.js
@@ -23,4 +23,30 @@ describe('PkemonOptions component', () => {
 
     })
 
+    test("debe de mostrar las 4 opciones correctamente", () => {
+
+        const liTags = wrapper.findAll('li')
+        expect(liTags.length).toBe(4) 
+
+        expect(liTags[0].text()).toBe('bulbasur')
+        expect(liTags[1].text()).toBe("ivysaur");
+        expect(liTags[2].text()).toBe("venusaur");
+        expect(liTags[3].text()).toBe("charmander");
+      
+    });
+
+    test("debe de emitir 'selection' con sus respectivos parámetros al hacer click", () => {
+      
+        const [li1, li2, li3, li4 ] = wrapper.findAll('li')
+
+        li1.trigger('click')
+
+        // console.log(expect(wrapper.emitted("selection")));
+        expect(wrapper.emitted("selection").length).toBe(4);
+        expect(wrapper.emitted("selection")[0]).toEqual([5]);
+        expect(wrapper.emitted("selection")[1]).toEqual([10]);
+        expect(wrapper.emitted("selection")[2]).toEqual([15]);
+        expect(wrapper.emitted("selection")[3]).toEqual([20]);
+    });
+
 })
