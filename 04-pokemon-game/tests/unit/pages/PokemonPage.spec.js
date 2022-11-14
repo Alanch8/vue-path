64bfe2e205d28e529ad7d71d01b4909405ec0e8b
Aitor Lancharro <aitor.lancharro@gmail.com>
2022-11-14 18:14:29 +0100

    Snapshot con data y stubs

--- 04-pokemon-game/tests/unit/pages/PokemonPage.spec.js
+++ 04-pokemon-game/tests/unit/pages/PokemonPage.spec.js
@@ -1,6 +1,8 @@
 import { shallowMount, mount } from '@vue/test-utils'
 import PokemonPage from "@/pages/PokemonPage";
 
+import pokemons from '../mocks/pokemons.mock';
+
 describe("PokemonPage component", () => {
 
     let wrapper
@@ -17,8 +19,24 @@ describe("PokemonPage component", () => {
 
         const mixPokemonArraySpy = jest.spyOn(PokemonPage.methods, 'mixPokemonArray')
         wrapper = shallowMount(PokemonPage);
-        
+
         expect(mixPokemonArraySpy).toHaveBeenCalled()
+    })
+
+    test('debe de hacer match con el snapshot cuando cargan los pokémons', () => {
+
+        const wrapper = shallowMount(PokemonPage, {
+            data() {
+                return {
+                  pokemonArr: pokemons,
+                  pokemon: null,
+                  showPokemon: false,
+                  showAnswer: false,
+                  message: "",
+                };
+            }
+        })
+        
     })
     
 });
