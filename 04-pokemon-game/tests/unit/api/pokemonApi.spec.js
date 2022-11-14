cc798f7d4867cbc43ebdac0c20600994a41ae0cb
Aitor Lancharro <aitor.lancharro@gmail.com>
2022-11-14 14:32:57 +0100

    Pruebas con Axios

---
+++ 04-pokemon-game/tests/unit/api/pokemonApi.spec.js
@@ -0,0 +1,15 @@
+import pokemonApi from "@/api/pokemonApi";
+
+describe('pokemonApi', () => {
+
+    test('axios debe de estar configurado con el api de pokémon', () => {
+
+        console.log(pokemonApi);
+
+        // expect(pokemonApi.defaults.baseURL).toBe(
+        //   "https://pokeapi.co/api/v2/pokemon"
+        // );
+
+    })
+
+})
