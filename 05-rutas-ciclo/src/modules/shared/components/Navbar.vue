ded90c5984ae8c6d9d0a00d53ddfb54884d474ea
Aitor Lancharro <aitor.lancharro@gmail.com>
2022-11-16 20:59:19 +0100

    RouterLink Personalizado

--- 05-rutas-ciclo/src/modules/shared/components/Navbar.vue
+++ 05-rutas-ciclo/src/modules/shared/components/Navbar.vue
@@ -1,25 +1,50 @@
 <template>
-    <div>
-        <router-link :to="{ name: 'home' }">Pokemon List</router-link>
+  <div>
+    <!-- <router-link :to="{ name: 'home' }">Pokemon List</router-link>
         <router-link :to="{ name: 'pokemon-id', params: { id: 85 } }">Pokemon por id</router-link>
-        <router-link :to="{ name: 'about' }">About</router-link>
-    </div>    
+        <router-link :to="{ name: 'about' }">About</router-link> -->
+    <CustomLink
+        v-for="link in links"
+        :key="link.to"
+        :link="link"    
+     />
+  </div>
 </template>
 
-<style scoped>
+<script>
+import { defineAsyncComponent } from 'vue'
 
-div { 
-    padding: 0 30px;
+export default {
+
+    data() {
+        return {
+            links: [
+                { to: '/home', name: 'Pokemons' },
+                { to: '/pokemonid/94', name: 'Por ID' },
+                { to: '/about', name: 'About' },
+                { to: 'https://google.com', name: 'Google' },
+            ]
+        }
+    },
+
+    components: {
+        CustomLink: defineAsyncComponent(() => import('./CustomLink.vue'))
+    }
+}
+</script>
+
+<style scoped>
+div {
+  padding: 0 30px;
 }
 
 div a {
-    font-weight: bold;
-    color: #2c3e50;
-    margin: 0 10px; 
+  font-weight: bold;
+  color: #2c3e50;
+  margin: 0 10px;
 }
 
-a.router-link-exact-active {
+/* a.router-link-exact-active {
     color: #42b983;
-}
-
+} */
 </style>
