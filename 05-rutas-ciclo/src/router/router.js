df8dfc06c254465f6676cf1b1fb182053763cd49
Aitor Lancharro <aitor.lancharro@gmail.com>
2022-11-17 15:56:01 +0100

    Segundo Layout

--- 05-rutas-ciclo/src/router/router.js
+++ 05-rutas-ciclo/src/router/router.js
@@ -43,8 +43,40 @@ const routes = [
         },
       },
       {
-        path: '',
-        redirect: { name: 'pokemon-about' }
+        path: "",
+        redirect: { name: "pokemon-about" },
+      },
+    ],
+  },
+
+  // DBZ Layout
+  {
+    path: "/dbz",
+    name: "dbz",
+    component: () =>
+      import(
+        /* webpackChunkName: "DragonBallLayout" */ "@/modules/dbz/layouts/DragonBallLayout"
+      ),
+    children: [
+      {
+        path: "characters",
+        name: "dbz-characters",
+        component: () =>
+          import(
+            /* webpackChunkName: "Characters" */ "@/modules/dbz/pages/Characters"
+          ),
+      },
+      {
+        path: "about",
+        name: "dbz-about",
+        component: () =>
+          import(
+            /* webpackChunkName: "About" */ "@/modules/dbz/pages/About"
+          ),
+      },
+      {
+        path: "",
+        redirect: { name: "dbz-characters" },
       },
     ],
   },
