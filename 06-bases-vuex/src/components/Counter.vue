cb0fac28f855ac8016cac29397a6f59e16234b51
Aitor Lancharro <aitor.lancharro@gmail.com>
2022-11-21 16:01:11 +0100

    Mutations

--- 06-bases-vuex/src/components/Counter.vue
+++ 06-bases-vuex/src/components/Counter.vue
@@ -2,6 +2,11 @@
   <h1>Counter - Vuex</h1>
   <h2>Direct access: {{ $store.state.count }}</h2>
   <h2>Computed: {{ countComputed }}</h2>
+
+  <button @click="increment">+1</button>
+  <button @click="incrementBy">+5</button>
+  <button>Random</button>
+
   <h1>mapState</h1>
   <h2>mapState: {{ count }}</h2>
   <h2>lastMutation: {{ lastMutation }}</h2>
@@ -24,5 +29,14 @@ export default {
     //   lastMutation: state => state.lastMutation
     // })
   },
+
+  methods: {
+    increment() {
+      this.$store.commit('increment') // fijate que increment() es un method en este component y 'increment' es la mutation del store. No tienen "nada"que ver. Los estamos relacionando entre ellos y usamos el mismo nombre.
+    },
+    incrementBy() {
+      this.$store.commit('incrementBy', 5)
+    }
+  }
 };
 </script>
--- 06-bases-vuex/src/store/index.js
+++ 06-bases-vuex/src/store/index.js
@@ -2,8 +2,19 @@ import { createStore } from "vuex";
 
 
 export default createStore({
-    state: {
+    state: { //similar a data()
         count: 1,
         lastMutation: 'none'
+    },
+
+    mutations: { //similar a methods(). Tienen que ser sincronas y son las que se encargan de cambiar el State. Prohibido hacer async await.
+        increment( state ) {
+            state.count++
+            state.lastMutation = 'increment'
+        },
+        incrementBy( state, val ) {
+            state.count += val
+            state.lastMutation = 'incrementBy'
+        }
     }
 })
