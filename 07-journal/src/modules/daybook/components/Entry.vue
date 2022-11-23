04dc164cee8ecaa74bf6e66ccf2900c0762109e0
Aitor Lancharro <aitor.lancharro@gmail.com>
2022-11-23 21:15:04 +0100

    EntryView

--- 07-journal/src/modules/daybook/components/Entry.vue
+++ 07-journal/src/modules/daybook/components/Entry.vue
@@ -1,12 +1,13 @@
 <template>
     <div 
         class="entry-container mb-3 pointer p-2"
+        @click="$router.push({ name: 'entry', params: { id: 10 }})"
     >
         <!-- Titulo -->
         <div class="entry-title d-flex">
-            <span class="text-success fs-5 fw-bold">15</span>
-            <span class="mx-1 fs-5">Julio</span>
-            <span class="mx-2 fw-light">2021, jueves </span>
+            <span class="text-success fs-5 fw-bold">23</span>
+            <span class="mx-1 fs-5">Noviembre</span>
+            <span class="mx-2 fw-light">2022, Miércoles </span>
         </div>
 
         <div class="entry-description">
