df1e0c2b8f214c56dbb7a038747a4921c47c78f2
Aitor Lancharro <aitor.lancharro@gmail.com>
2022-11-24 11:02:18 +0100

    Mini tarea - Fab icon

--- 07-journal/src/modules/daybook/views/EntryView.vue
+++ 07-journal/src/modules/daybook/views/EntryView.vue
@@ -26,7 +26,9 @@
             placeholder="Qué sucedió hoy?"></textarea>
     </div>
 
-    <Fab />
+    <Fab
+        icon="fa-save"
+    />
 
     <img 
         src="https://images.unsplash.com/photo-1506905925346-21bda4d32df4?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80" 
