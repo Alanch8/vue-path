746ab5b09a386031cc2c873e76990298a65b6ce9
Aitor Lancharro <aitor.lancharro@gmail.com>
2023-01-10 12:04:38 +0100

    Peticiones http

--- 08-compo-api/src/router/index.js
+++ 08-compo-api/src/router/index.js
@@ -25,6 +25,15 @@ const routes = [
     component: () =>
       import(/* webpackChunkName: "counter" */ "../views/CounterView.vue"),
   },
+  {
+    path: "/users",
+    name: "users",
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () =>
+      import(/* webpackChunkName: "users" */ "../views/UsersView.vue"),
+  },
 ];
 
 const router = createRouter({
