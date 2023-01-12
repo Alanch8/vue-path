4c999629b1ddb9d5174b63d20379a0a062307699
Aitor Lancharro <aitor.lancharro@gmail.com>
2023-01-12 13:56:41 +0100

    Mostrar los usuarios y conectar la paginación

--- 08-compo-api/src/views/UsersView.vue
+++ 08-compo-api/src/views/UsersView.vue
@@ -1,20 +1,20 @@
 <template>
-  <h2>Espere por favor...</h2>
-  <h2>Usuarios</h2>
-  <h5>Error en la carga...</h5>
+  <h2 v-if="isLoading">Espere por favor...</h2>
+  <h2 v-else>Usuarios</h2>
+  <h5 v-if="errorMessage">{{errorMessage}}</h5>
 
-    <div>
+    <div v-if="users.length > 0">
       <ul>
-        <li>
-          <h4>Nombre de la persona</h4>
-          <h6>email@gmail.com</h6>
+        <li v-for="{ first_name, last_name, email, id } in users" :key="id">
+          <h4>{{ first_name }} {{ last_name }}</h4>
+          <h6>{{ email }}</h6>
         </li>
       </ul>
     </div>
 
-    <button>Atrás</button>
-    <button>Siguiente</button>
-    <span>Páginas: 5</span>
+    <button @click="prevPage">Atrás</button>
+    <button @click="nextPage">Siguiente</button>
+    <span>Páginas: {{ currentPage }}</span>
 
 </template>
 
@@ -27,7 +27,7 @@ export default {
     const users = ref([])
     const isLoading = ref(true)
     const currentPage = ref(1)
-    const errorMessage = ref(1)
+    const errorMessage = ref()
 
     const getUsers = async( page = 1) => {
 
@@ -38,16 +38,30 @@ export default {
       const { data } = await axios.get('https://reqres.in/api/users', {
         params: { page }
       })
-      if (data.data.leng > 0) {
+      if (data.data.length > 0) {
         users.value = data.data
         currentPage.value = page
+        errorMessage.value = null
       } else if ( currentPage.value > 0 ) {
         errorMessage.value = 'No hay más usuarios'
       }
 
+      isLoading.value = false
+
     }
 
     getUsers()
+
+    return {
+      currentPage,
+      errorMessage,
+      isLoading,
+      users,
+
+      nextPage: () => getUsers( currentPage.value + 1  ),
+      prevPage: () => getUsers( currentPage.value - 1  ),
+
+    }
 
   }
 }
