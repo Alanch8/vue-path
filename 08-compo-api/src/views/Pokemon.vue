eed9d4f35de027973619fb03f0dfc9ac72fcbc4f
Aitor Lancharro <aitor.lancharro@gmail.com>
2023-01-15 13:38:55 +0100

    Bloquear la salida de una ruta

--- 08-compo-api/src/views/Pokemon.vue
+++ 08-compo-api/src/views/Pokemon.vue
@@ -3,13 +3,11 @@
   <h1 v-else-if="errorMessage">{{ errorMessage }}</h1>
 
   <template v-else>
-      <h3>{{ pokemon.name }}</h3>
-      <img :src="pokemon.sprites.front_default" :alt="pokemon.name">
-      <br>
-      <router-link :to="{ name: 'pokemon-search'}">Regresar</router-link>
-      
+    <h3>{{ pokemon.name }}</h3>
+    <img :src="pokemon.sprites.front_default" :alt="pokemon.name" />
+    <br />
+    <router-link :to="{ name: 'pokemon-search' }">Regresar</router-link>
   </template>
-
 </template>
 
 <script>
@@ -18,33 +16,29 @@ import { useRoute, onBeforeRouteLeave } from "vue-router";
 import usePokemon from "@/composables/usePokemon";
 
 export default {
-
   setup() {
-
-    const route = useRoute()
+    const route = useRoute();
     // console.log(route.params.id)
-    const { errorMessage, isLoading, pokemon, searchPokemon } = usePokemon( route.params.id )
+    const { errorMessage, isLoading, pokemon, searchPokemon } = usePokemon(
+      route.params.id
+    );
 
     watch(
       () => route.params.id,
       () => searchPokemon(route.params.id)
-    )
+    );
 
-    onBeforeRouteLeave() => {
-      
-      const answer = window.confirm('¿Está seguro que desea salir?')
+    onBeforeRouteLeave(() => {
+      const answer = window.confirm("¿Está seguro que desea salir?");
 
-      if ( !answer ) return false // false, bloquea la salida
-        
-    }
+      if (!answer) return false; // false, bloquea la salida
+    });
 
     return {
       errorMessage,
       isLoading,
       pokemon,
-    }
-
-  }
-
-}
+    };
+  },
+};
 </script>
