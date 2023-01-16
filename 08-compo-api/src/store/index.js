8b39be49a020d013b389acc04f5f84923c7c9b0c
Aitor Lancharro <aitor.lancharro@gmail.com>
2023-01-16 16:47:35 +0100

    Diseño del State

--- 08-compo-api/src/store/index.js
+++ 08-compo-api/src/store/index.js
@@ -2,13 +2,16 @@ import { createStore } from 'vuex'
 
 export default createStore({
   state: {
+    todos: [
+      { id: 1, text: "Recolectar las piedras del infinito", completed: false },
+      { id: 2, text: "Piedra del alma", completed: false },
+      { id: 3, text: "Piedra de poder", completed: false },
+      { id: 4, text: "Piedra de realidad", completed: false },
+      { id: 5, text: "Conseguir nuevos secuaces competentes", completed: false },
+    ],
   },
-  getters: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
-  modules: {
-  }
-})
+  getters: {},
+  mutations: {},
+  actions: {},
+  modules: {},
+});
