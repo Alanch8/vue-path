382c73bf3af0250c0c9504fa552e458f225f81d3
Aitor Lancharro <aitor.lancharro@gmail.com>
2023-01-16 18:43:45 +0100

    Tarea - Crear y usar getters

--- 08-compo-api/src/store/index.js
+++ 08-compo-api/src/store/index.js
@@ -7,16 +7,26 @@ export default createStore({
       { id: 2, text: "Piedra del alma", completed: false },
       { id: 3, text: "Piedra de poder", completed: false },
       { id: 4, text: "Piedra de realidad", completed: false },
-      { id: 5, text: "Conseguir nuevos secuaces competentes", completed: false },
+      {
+        id: 5,
+        text: "Conseguir nuevos secuaces competentes",
+        completed: false,
+      },
     ],
   },
   getters: {},
   mutations: {},
   actions: {},
   getters: {
-    pendingTools( state, getters, rootState ) {
-      return state.todos.filter( todo => todo.completed )
-    }
+    pendingTools(state, getters, rootState) {
+      return state.todos.filter((todo) => !todo.completed);
+    },
+    allTodos: (state, getters, rootState) => {
+      return state.todos;
+    },
+    completedTodos: (state, getters, rootState) => {
+      return state.todos.filter((todo) => todo.completed);
+    },
   },
   modules: {},
 });
