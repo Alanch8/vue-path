b3aa0741b7f2fed046e8a09ab32b4817f2a8b4d9
Aitor Lancharro <aitor.lancharro@gmail.com>
2023-01-24 22:38:43 +0100

    Slots y NameSlots

--- 08-compo-api/src/views/CustomSlots.vue
+++ 08-compo-api/src/views/CustomSlots.vue
@@ -2,20 +2,28 @@
   <h1>Aprendiendo Slots</h1>
   <button>Abrir Modal</button>
 
-  <Modal />
-
+  <modal title="Hola mundo">
+    <!-- <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit!</p>
+    <h2>Titulo del modal</h2>
+    <button>Salir</button> -->
+    <template v-slot:footer>
+      <button>Salir</button>
+    </template>
+    <template v-slot:body>
+      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit!</p>
+    </template>
+    <template v-slot:header>
+      <h2>Titulo del modal</h2>
+    </template>
+  </modal>
 </template>
 
 <script>
 import Modal from "../components/Modal.vue";
 export default {
-    components: { Modal },
-    setup(){
-
-    }
-}
+  components: { Modal },
+  setup() {},
+};
 </script>
 
-<style>
-
-</style>
+<style></style>
